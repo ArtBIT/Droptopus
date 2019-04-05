c1c73e26668d89ed974f08528dbbbb23a93902c5
Djordje Ungar <djordje.ungar@gmail.com>
2019-04-05 10:17:09 +0200

    Cleanup the test a bit

--- tests/test_target_handling.py
+++ tests/test_target_handling.py
@@ -1,40 +1,72 @@
+"""
+This is module for testing the most basic functionality of Droptopus
+"""
 import sys
 import unittest
-from PyQt5.QtWidgets import (
-        QApplication,
-        QWidget
-)
-from PyQt5.QtTest import QTest
-from PyQt5.QtCore import Qt
-from os.path import isfile, isdir, join
-from droptopus import settings, config
+from os.path import isfile, join
+from PyQt5.QtWidgets import QWidget, QApplication
 from droptopus.widgets import FileTarget, DirTarget, CreateTarget
+from droptopus import config
 
-app = QApplication(sys.argv)
 
 class TestDropTarget(unittest.TestCase):
-    def setUp(self):
-        '''Create the widgets'''
-        self.widget = QWidget()
-        self.create_target = self.instantiateWidget({"desc": "", "type":"builtin", "name": "Create Action", "path": "", "icon": join(config.ASSETS_DIR, 'plus_white.png'), "index": 0})
-        self.dir_target    = self.instantiateWidget({"desc": "", "type":"dir", "name": "Dir Action", "path": "/tmp", "icon": join(config.ASSETS_DIR, 'plus_white.png'), "index": 1})
-        self.file_target   = self.instantiateWidget({"desc": "", "type":"file", "name": "File Action", "path": "echo", "icon": join(config.ASSETS_DIR, 'plus_white.png'), "index": 2})
+    """Test for Droptopus URL parsing"""
 
-    def instantiateWidget(self, conf):
-        '''Helper method to create the widgets'''
-        widget_type = conf['type']
-        if widget_type == 'dir':
+    def setUp(self):
+        """Create the widgets"""
+        app = QApplication(sys.argv)
+        self.widget = QWidget()
+        self.create_target = self.instantiate_widget(
+            {
+                "desc": "",
+                "type": "builtin",
+                "name": "Create Action",
+                "path": "",
+                "icon": join(config.ASSETS_DIR, "plus_white.png"),
+                "index": 0,
+            }
+        )
+        self.dir_target = self.instantiate_widget(
+            {
+                "desc": "",
+                "type": "dir",
+                "name": "Dir Action",
+                "path": "/tmp",
+                "icon": join(config.ASSETS_DIR, "plus_white.png"),
+                "index": 1,
+            }
+        )
+        self.file_target = self.instantiate_widget(
+            {
+                "desc": "",
+                "type": "file",
+                "name": "File Action",
+                "path": "echo",
+                "icon": join(config.ASSETS_DIR, "plus_white.png"),
+                "index": 2,
+            }
+        )
+
+    def instantiate_widget(self, conf):
+        """Helper method to create the widgets"""
+        widget_type = conf["type"]
+
+        if widget_type == "dir":
             return DirTarget(self.widget, conf)
-        elif widget_type == 'file':
+
+        if widget_type == "file":
             return FileTarget(self.widget, conf)
-        elif widget_type == 'builtin':
+
+        if widget_type == "builtin":
             return CreateTarget(self.widget, conf)
 
+        return None
+
     def test_handle_urls(self):
-        '''Test URL handling'''
+        """Test URL handling"""
         urls = [
-            'https://i.imgur.com/7VEHUX9.jpg',
-            u'http://www.example.com/düsseldorf?neighbourhood=Lörick',
+            "https://i.imgur.com/7VEHUX9.jpg",
+            u"http://www.example.com/düsseldorf?neighbourhood=Lörick",
         ]
         for url in urls:
             status, dest_file = self.dir_target.handle(url)
@@ -44,3 +76,5 @@ class TestDropTarget(unittest.TestCase):
             status, dest_file = self.file_target.handle(url)
             self.assertEqual(0, status)
 
+if __name__ == '__main__':
+    unittest.main()
